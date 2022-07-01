8148648255a7669a1d1a5989465318fd2abcf475
Jorge Kimura <kimurajorge2001@gmail.com>
2022-07-01 14:49:03 -0300

    Variavel conjuntoDados

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 import app from './models/classes.js'
 import Requisicao from '../src/controllers/Requisicao.js'
 
-console.log(await Requisicao.pegarDados())
+const conjutoDados = await Requisicao.pegarDados()
+
+console.log(conjutoDados)
