03c288d7f290770ec8723d4406d1281a96a92186
Jorge Kimura <kimurajorge2001@gmail.com>
2022-07-02 18:29:40 -0300

    develop

--- src/script.js
+++ src/script.js
@@ -1,11 +1,10 @@
-import { noticias } from "./controllers/criacaoCards.js"
-import { noticiaPesquisada } from "./controllers/criacaoCards.js"
+import { noticias } from "./controllers/criacaoCards.js";
+import { noticiaPesquisada } from "./controllers/criacaoCards.js";
 
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 const divMaterias = document.querySelector(".card__adjacentes");
 divMaterias.append(...noticias);
 
-main.append(noticiaPesquisada('3'))
+main.append(noticiaPesquisada("3"));
 
-main.append(divMaterias)
-
+main.append(divMaterias);
