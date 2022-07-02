7dee0bbfec3a8434e80c9de27c168c2e9d72eb38
Jorge Kimura <kimurajorge2001@gmail.com>
2022-07-02 14:24:40 -0300

    Criação de divs na main

--- src/models/CardPrincipal.js
+++ src/models/CardPrincipal.js
@@ -1,25 +1,21 @@
 export default class CardPrincipal{
     static criarCardPrincipal(materia){
       
-      const materiaPrincipal = [...materia.childNodes]
-      const [img, span, h1, p, h3] = materiaPrincipal
-      console.dir(span)
-      console.dir(h3)
-      const divMateria = document.createElement('div');
+      const divMateriaPrincipal = document.querySelector('.card__principal');
       
+      const categoria = document.createElement('span');
       const titulo = document.createElement('h1');
       const resumo = document.createElement('p');
       const fonte = document.createElement('h3');
       const imagem = document.createElement('img');
-      const categoria = document.createElement('span');
-
-      categoria.innerText = `${h3.innerText}`;
+      
+      categoria.innerText = `${materia.categoria}`;
       titulo.innerText = `${h1.innerText}`;
       resumo.innerText = `${p.innerText}`;
       fonte.innerText = `Fonte: ${span.innerText}.`;
       imagem.src = `${img.src}`;
     
-      divMateria.append(categoria, titulo, resumo, fonte, imagem);
-      return divMateria
+      divMateriaPrincipal.append(categoria, titulo, resumo, fonte, imagem);
+      return divMateriaPrincipal
     }
 }
